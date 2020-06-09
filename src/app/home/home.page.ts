59f83e14f7abdf74e433b1500fc2f22660152f45
KkyJim12 <jirakarnjim1@gmail.com>
2020-06-09 18:23:28 +0700

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
+  todayStats;
 
+  ngOnInit() {
+    this.covidToday();
+  }
+
+  covidToday() {
+    this.http
+      .get("https://covid19.th-stat.com/api/open/today")
+      .subscribe((data) => {
+        this.todayStats = data;
+        console.log(this.todayStats);
+      });
+  }
 }
